7d8d23843c0c4a5cc02e1b1e48c4baf1744000f7
jchhikara <jatinn09@gmail.com>
2015-07-07 12:52:52 -0700

    V2

--- data/js/contextSearchResult.js
+++ data/js/contextSearchResult.js
@@ -1,101 +1,110 @@
-$(function () {
-    addon.port.on("showt", function (text) {
-            // get info from wiki for selection text
-            getContent("http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=" + text + "&format=json&exintro=1&redirects=",
-                function (data) {
-                    var resultData = data.query.pages, firstPage;
-                    $.each(resultData, function (i, item) {
-                        firstPage = i;
-                        return;
-                    });
+'use strict';
 
-                    $("#wiki").find(".content").html(data.query.pages[firstPage].extract).end().find(".more").off("click")
-                        .on("click", function () {
-                            addon.port.emit("hideP", true);
+$(function () {
+    addon.port.on('showt', function (text) {
+        // get info from wiki for selection text, the exintro part is removed from the query as sometime it gives no rest, for example maker
+        getContent('http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=' + text + '&format=json&redirects=',
+            function (data) {
+                var resultData = data.query.pages, firstPage;
+                $.each(resultData, (i) => {
+                    firstPage = i;
+                     false;
+                });
+                var wikiResult = data.query.pages[firstPage].extract;
+                if (wikiResult) {
+                    $('#wiki').find('.content').html(data.query.pages[firstPage].extract).end().find('.more').off('click')
+                        .on('click', () => {
+                            addon.port.emit('hideP', true);
                             //add animation for the box
                             //setTimeout(function(){}, 6000);
-                            window.open("https://en.wikipedia.org/wiki/" + text);
+                            window.open('https://en.wikipedia.org/wiki/' + text);
                         });
-                });
-
-// get definition from merriam-webster for selection text
-            var site = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + text + "?key=19366857-7e67-4db8-977e-6d613ef8a8df";
-            var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + site + '"') + '&format=xml&callback=?';
-
-            getContent(yql, function (data) {
-                var definitionXml = $.parseXML(data.results[0]),
-                    $xml = $(definitionXml);
-                $("#selectionType").text($xml.find("fl").eq(0).text());
-                var $serialNumber = $xml.find("def").find("sn");
-                var meaningCount = $serialNumber.length;
-                var $defList = $xml.find("def").find("dt");
-                var $listItems = $();
-
-                for (var i = 0; i < meaningCount; i++) {
-                    if (isNaN($serialNumber.eq(i).text().charAt(0)) == false) {
-                        $listItems = $listItems.add($("<li>", {"style": " margin-left:5px"}).text($defList.eq(i).text()))
-                    }
-                    else {
-                        var lastChild = $listItems[$listItems.length - 1];
-                        if (lastChild) {
-                            lastChild = $(lastChild);
-                            if (lastChild.children().length == 0) {
-                                lastChild.append($("<ul>", {"style": " list-style: outside none none"}).append($("<li>", {"style": " list-style: outside none none"}).text($defList.eq(i).text())));
-                            }
-                            else {
-                                lastChild.find("ul").append($("<li>", {"style": " list-style: outside none none"}).text($defList.eq(i).text()));
-                            }
-                        }
-                    }
-                    //  $listItems = $listItems.add($("<li>").text($defList.eq(i).text()));
                 }
-                $("#meaningList").append($listItems);
-                $("#definition").find(".more").off("click")
-                    .on("click", function () {
-                        addon.port.emit("hideP", true);
-                        //add animation for the box
-                        //setTimeout(function(){}, 6000);
-                        window.open("http://www.merriam-webster.com/dictionary/" + text, "_blank");
-                    });
+                else {
+                    $('#wiki').find('.content').html(`Sorry, Wikipedia does not have an article with this exact name as <b>"` + text + `"</b>.`)
+                }
             });
 
-// get shop data from ebay
-            getContent("http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=Mycomp927-53f9-4def-999b-94baba31191&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + text + "&paginationInput.entriesPerPage=2",
-                function (data) {
+// get definition from merriam-webster for selection text
+        var site = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + text + '?key=19366857-7e67-4db8-977e-6d613ef8a8df';
+        var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + site + '"') + '&format=xml&callback=?';
 
-                    var items = data.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-                    var $listTemplate = $("#shop-list").find("tfoot").children().clone();
-                    var $list = $();
+        getContent(yql, function (data) {
+            var definitionXml = $.parseXML(data.results[0]),
+                $xml = $(definitionXml);
+            $('#selectionType').text($xml.find('fl').eq(0).text());
+            var $serialNumber = $xml.find('def').find('sn');
+            var meaningCount = $serialNumber.length;
+            var $defList = $xml.find('def').find('dt');
+            var $listItems = $();
 
-
-                    for (var i = 0; i < items.length; ++i) {
-                        var item = items[i], $currentItem = $listTemplate.clone();
-                        var title = item.title;
-                        var pic = item.galleryURL;
-                        var viewItem = item.viewItemURL;
-
-                        if (null != title && null != viewItem) {
-                            $list = $list.add($currentItem.find("img").attr("src", pic).css({
-                                    height: "50px",
-                                    width: "50px"
-                                }).end()
-                                    .find(".price").text(item.sellingStatus[0].currentPrice[0].__value__).end()
-                                    .find(".title").text(title).attr("href", viewItem).end()
-                            );
-
+            for (var i = 0; i < meaningCount; i++) {
+                if (isNaN($serialNumber.eq(i).text().charAt(0)) === false) {
+                    $listItems = $listItems.add($('<li>', {'style': ' margin-left:5px'}).text($defList.eq(i).text()));
+                }
+                else {
+                    var lastChild = $listItems[$listItems.length - 1];
+                    if (lastChild) {
+                        lastChild = $(lastChild);
+                        if (lastChild.children().length === 0) {
+                            lastChild.append($('<ul>', {'style': ' list-style: outside none none'}).append($('<li>', {'style': ' list-style: outside none none'}).text($defList.eq(i).text())));
+                        }
+                        else {
+                            lastChild.find('ul').append($('<li>', {'style': ' list-style: outside none none'}).text($defList.eq(i).text()));
                         }
                     }
+                }
+                //  $listItems = $listItems.add($('<li>').text($defList.eq(i).text()));
+            }
+            if (($xml.find('fl').eq(0).text() !== '' ) || $listItems.length) {
 
-                    $("#shop-list").find("tbody").empty().append($list);
-                    $("#shop").find(".more").off("click")
-                        .on("click", function () {
-                            addon.port.emit("hideP", true);
-                            //add animation for the box
-                            //setTimeout(function(){}, 6000);
-                            window.open("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR10.TRC0.A0.H0.X" + text + ".TRS0&_nkw=" + text + "&_sacat=0");
-                        });
-                });
+                $('#meaningList').empty().append($listItems);
+                $('#definition').find('.more').off('click')
+                    .on('click', () => {
+                        addon.port.emit('hideP', true);
+                        //add animation for the box
+                        //setTimeout(function(){}, 6000);
+                        window.open('http://www.merriam-webster.com/dictionary/' + text, '_blank');
+                    });
+            }
+            else {
+                $('#meaningList').empty().append($('<li>', {'style': ' list-style: outside none none;margin-left:-15px'}).text(`The word you've entered isn't in the dictionary.`));
+            }
         });
+
+// get shop data from ebay
+        getContent('http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=Mycomp927-53f9-4def-999b-94baba31191&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + text + '&paginationInput.entriesPerPage=2',
+            function (data) {
+                var items = data.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+                var $listTemplate = $('#shop-list').find('tfoot').children().clone();
+                var $list = $();
+                for (var i = 0; i < items.length; ++i) {
+                    var item = items[i], $currentItem = $listTemplate.clone();
+                    var title = item.title;
+                    var pic = item.galleryURL;
+                    var viewItem = item.viewItemURL;
+
+                    if (null !== title && null !== viewItem) {
+                        $list = $list.add($currentItem.find('img').attr('src', pic).css({
+                                height: '50px',
+                                width: '50px'
+                            }).end()
+                                .find('.price').text(item.sellingStatus[0].currentPrice[0].__value__).end()
+                                .find('.title').text(title).attr('href', viewItem).end()
+                        );
+                    }
+                }
+
+                $('#shop-list').find('tbody').empty().append($list);
+                $('#shop').find('.more').off('click')
+                    .on('click', function () {
+                        addon.port.emit('hideP', true);
+                        //add animation for the box
+                        //setTimeout(function(){}, 6000);
+                        window.open('http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR10.TRC0.A0.H0.X' + text + '.TRS0&_nkw=' + text + '&_sacat=0');
+                    });
+            });
+    });
     var el = document.getElementById('nav-tab'); // used vanilla js for tabs, just to keep it simple for tabs
     el.addEventListener('click', onTabClick, false);
 });
@@ -103,15 +112,15 @@ $(function () {
 
 function getContent(url, callbackSuccess, callbackError) {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: url,
-        dataType: "jsonp",
+        dataType: 'jsonp',
         cache: false,
         crossDomain: true,
         processData: true,
         success: callbackSuccess,
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log("We think something went wrong :( - " + errorThrown);
+            console.log('We think something went wrong :( - ' + errorThrown);
         }
     });
 }
--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-var self = require('sdk/self'), data = self.data, selection = require("sdk/selection"), panels = require("sdk/panel");
+'use strict';
 
-var browserWindow = require("sdk/window/utils").getMostRecentBrowserWindow(), browserDocument = browserWindow.document, doc = browserWindow.content.document;
-//var selectedText = "";
-var clientXDoc = 0, clientYDoc = 0, selectedPhrase = "";
-
-browserWindow.addEventListener("scroll", function (eve) {
-    var miniD = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector("#content"), "id", "minimizerDots");
-    if (miniD) {
-        var xulStackB = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector("#content"), "class", "browserStack");
-        // instead of removing position can be changed but changing top, left does not work :(
-        xulStackB.removeChild(miniD);
-    }
-});
-
+var self = require('sdk/self');
+var data = self.data;
+var selection = require('sdk/selection');
+var panels = require('sdk/panel');
+var windows = require('sdk/window/utils');
+var browserWindow = '';
+var browserDocument = '';
+var clientXDoc = 0;
+var clientYDoc = 0;
+var selectedPhrase = '';
 
 function myListener() {
+    console.log(browserDocument.title);
     selectedPhrase = selection.text;
-    var selRange;
     var sel = browserWindow.content.getSelection();
     if (sel.rangeCount) {
-        selRange = sel.getRangeAt(sel.rangeCount - 1);
-        selRange.collapse();
-        var martyrElem = browserDocument.createElement("span");
-        selRange.insertNode(martyrElem);
-        var rect = martyrElem.getBoundingClientRect();
-        clientXDoc = rect.left;
-        clientYDoc = rect.top;
-        martyrElem.parentNode.removeChild(martyrElem);
+        let selRange = sel.getRangeAt(sel.rangeCount - 1);
+        let rect = selRange.getBoundingClientRect();
+        clientXDoc = rect.left + (rect.right - rect.left);
+        clientYDoc = rect.top + rect.height;
     }
-    if (selectedPhrase != "" && selectedPhrase)
+    if (selectedPhrase !== '' && selectedPhrase) {
         showSearchContext(selectedPhrase, clientXDoc, clientYDoc);
+    }
 }
-
-selection.on('select', myListener);
 
 
 function panelHide() {
@@ -40,80 +32,78 @@ function panelHide() {
 
 }
 
-var needNewPanel = false;
-
+var fetchFresh = false;
 function showSearchContext(selectedText, x, y) {
 
 // Add New panel
-
-    var minimizerDots = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector("#content"), "id", "minimizerDots");//browserDocument.getElementById("minimizerDots");
-    console.log(minimizerDots);
+    var minimizerDots = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector('#content'), 'id', 'minimizerDots');//browserDocument.getElementById('minimizerDots');
+    //console.log(minimizerDots);
     selectedPhrase = selectedText;
     if (minimizerDots == null) {
         maker(selectedText, x, y);
     }
     else {
-        var xulStackB = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector("#content"), "class", "browserStack");
-        minimizerDots.setAttribute("top", y);
-        minimizerDots.setAttribute("left", x);
-        needNewPanel = true;
+        minimizerDots.setAttribute('top', y);
+        minimizerDots.setAttribute('left', x);
+        fetchFresh = true;
     }
 }
 
-var searchResultPanel = null, counter = 0; // counter used for handle cache in the panel
-
+var searchResultPanel = null;
 function maker(selectedText, x, y) {
-    var iconStack = browserDocument.createElement("image");
-    iconStack.setAttribute("src", self.data.url("context-search-indicator.svg"));
-    iconStack.setAttribute("top", y);
-    iconStack.setAttribute("left", x);
-    iconStack.setAttribute("height", "30");
-    iconStack.setAttribute("width", "30");
-    iconStack.setAttribute("id", "minimizerDots");
-
-
-    var xulStackBro = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector("#content"), "class", "browserStack");
+    var iconStack = browserDocument.createElement('image');
+    iconStack.setAttribute('src', self.data.url('context-search-indicator.svg'));
+    iconStack.setAttribute('top', y);
+    iconStack.setAttribute('left', x);
+    iconStack.setAttribute('height', '30');
+    iconStack.setAttribute('width', '30');
+    iconStack.setAttribute('id', 'minimizerDots');
+    var xulStackBro = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector('#content'), 'class', 'browserStack');
     xulStackBro.appendChild(iconStack);
 
     if (searchResultPanel == null) {
-        needNewPanel = true;
+        fetchFresh = true;
         searchResultPanel = panels.Panel({
             width: 400,
-            height: 280
+            height: 280,
+            contentURL: data.url('context-search-results.html?t=')
             , onHide: panelHide
         });
-        searchResultPanel.on("show", function () {
-            searchResultPanel.port.emit("showt", selectedPhrase);
+        searchResultPanel.on('show', () => {
+            searchResultPanel.port.emit('showt', selectedPhrase);
         });
-
-
-        searchResultPanel.port.on("hideP", function () {
+        searchResultPanel.port.on('hideP', () => {
             searchResultPanel.hide();
-
         });
     }
 
-
-    iconStack.addEventListener("click", function () {
+    iconStack.addEventListener('click', () => {
         if (searchResultPanel.isShowing) {
             searchResultPanel.hide();
-            needNewPanel = true;
+            fetchFresh = true;
         }
         else {
-            if (needNewPanel) {
-                searchResultPanel.contentURL = data.url("context-search-results.html?t=" + counter);
-
-                counter += 1;
-                needNewPanel = false;
+            if (fetchFresh) {
+                searchResultPanel.port.emit('showt', selectedPhrase);
+                fetchFresh = !fetchFresh;
             }
             searchResultPanel.show({position: {top: clientYDoc, left: clientXDoc}});
         }
     });
-
 }
 
-function dummy(text, callback) {
-    callback(text);
-}
 
-exports.dummy = dummy;
+exports.main = () => {
+    selection.on('select', myListener);
+    browserWindow = windows.getMostRecentBrowserWindow();
+    browserDocument = browserWindow.document;
+    browserWindow.addEventListener('scroll', () => {
+        var miniD = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector('#content'), 'id', 'minimizerDots');
+        if (miniD) {
+            let xulStackB = browserDocument.getAnonymousElementByAttribute(browserDocument.querySelector('#content'), 'class', 'browserStack');
+            // instead of removing position can be changed but changing top, left does not work :(
+            xulStackB.removeChild(miniD);
+        }
+    });
+    console.log('asd');
+};
